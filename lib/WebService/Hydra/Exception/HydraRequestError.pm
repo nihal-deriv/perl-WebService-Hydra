d33c6a483b02f33b5025fecf780041c78d90f9c3
hani-deriv <hani@deriv.com>
2024-09-13 08:48:00 +0000

    init sdk

---
+++ lib/WebService/Hydra/Exception/HydraRequestError.pm
@@ -0,0 +1,22 @@
+package WebService::Hydra::Exception::HydraRequestError;
+use strict;
+use warnings;
+use Object::Pad;
+
+## VERSION
+
+class WebService::Hydra::Exception::HydraRequestError :isa(WebService::Hydra::Exception) {
+    
+
+    sub BUILDARGS {
+        my ($class, %args) = @_;
+
+        $args{message}  //= 'Sorry, something went wrong while processing your request';
+        $args{category} //= 'hydra';
+        
+        return %args;
+    }
+}
+
+
+1;
